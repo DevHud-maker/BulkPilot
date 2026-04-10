45b8b65ab18303e65d03b28388e92dd16de091f9
Nik Dev <amazinprodcts@gmail.com>
2026-04-10 16:12:27 +0200

    deploy:9

---
+++ app/routes/webhooks.customers.redact.jsx
@@ -0,0 +1,13 @@
+import { authenticate } from "../shopify.server";
+
+export const action = async ({ request }) => {
+  const { topic, shop, payload } = await authenticate.webhook(request);
+
+  console.log("Compliance webhook received:", {
+    topic,
+    shop,
+    payload,
+  });
+
+  return new Response();
+};
